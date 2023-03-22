4943e9ec4af9b5c5b355a9f6eb13ca838e4a229c
LieuxC <wshwlink@qq.com>
2023-03-22 12:56:52 +0800

    Upgrade Chapter2

---
+++ Chapter2/2_2_UnitTrans.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Shiwei Wang on 2023/3/22.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float distance;
+    cout << "Please enter distance (long): ";
+    cin >> distance;
+    cout << "Transformed distance: " << distance * 220 << endl;
+    return 0;
+}
