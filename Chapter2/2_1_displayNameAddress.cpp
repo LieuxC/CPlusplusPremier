0e6cfcaf965135629205a6ea5920fa07d1baf707
Shiwei Wang <shiweiw.@ShiweideMacBook-Pro.local>
2023-03-22 12:18:51 +0800

    Chapter2 first commit

---
+++ Chapter2/2_1_displayNameAddress.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Shiwei Wang on 2023/3/19.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "My Name: " << "LieuxC\n";
+    cout << "My Address: " << "China" << endl;
+    return 0;
+}
